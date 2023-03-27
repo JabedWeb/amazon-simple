49339ff353b74c173bfbe0b0f28fd234038fdf60
jabed366 <jabedhasan231@gmail.com>
2023-03-27 23:58:08 +0600

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
+import './Header.css'
+
+// logo from images folder
+
+import logo from '../../images/Logo.svg'
+
 const Header = () => {
   return (
-    <h1>Header</h1>
+    <div className='header'>
+        <img src={logo} alt="" />
+        <nav>
+          <a href="order">Order</a>
+          <a href="Order Review">Order Review</a>
+          <a href="Manage Inventory">Manage Inventory</a>
+          <a href="Login">Login</a>
+        </nav>
+    </div>
   )
 }
 
