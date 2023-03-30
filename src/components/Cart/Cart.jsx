3d7edac03291d8836d275c86e2dfd76bcba77533
jabed366 <jabedhasan231@gmail.com>
2023-03-30 21:22:38 +0600

    Completed Project

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -9,17 +9,20 @@ const Cart = (props) => {
 
     let total=0;
     let shipping_total=0;
+    let quantity=0;
 
     for(const product of cart){
-        total=total+product.price
+        product.quantity=product.quantity || 1;
+        total=total+product.price*product.quantity
         shipping_total=shipping_total+product.shipping
+        quantity=quantity+product.quantity;
     }
     const tax=total*7/100
     const grand_total=total+shipping_total+tax
   return (
    <div className="cart">
         <h4>Order Summary</h4>
-        <h6>Selected Item : {cart.length}</h6>
+        <h6>Selected Item : {quantity}</h6>
         <h6>Total Price : {total}</h6>
         <h6>Total Shipping : {shipping_total}</h6>
         <h6>Tax : {tax}</h6>
